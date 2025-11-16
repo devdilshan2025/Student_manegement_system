1c76d053ad0e3ad40eecdcab5eb35489d331a212
devdilshan2025 <dsbandara4@gmail.com>
2025-11-16 10:34:29 +0530

    create add student API

--- src/main/java/edu/icet/Controller/StudentController.java
+++ src/main/java/edu/icet/Controller/StudentController.java
@@ -1,4 +1,22 @@
 package edu.icet.Controller;
 
+import edu.icet.model.dto.Student;
+import edu.icet.repository.StudentRepository;
+import edu.icet.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/add")
+    public void add(@RequestBody Student student){
+
+        studentService.add(student);
+    }
+
+
 }
--- src/main/java/edu/icet/service/StudentService.java
+++ src/main/java/edu/icet/service/StudentService.java
@@ -1,7 +1,25 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.Student;
+import edu.icet.model.entity.StudentEntity;
+import edu.icet.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public void add(Student student){
+        studentRepository.save(new StudentEntity(
+                student.getId(),
+                student.getName(),
+                student.getAddress(),
+                student.getEmail(),
+                student.getPhone(),
+                student.getBirthday()
+        ));
+    }
 }
