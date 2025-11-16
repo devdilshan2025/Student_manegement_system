291919b0c527b062fe6bfe41ce657d36027b8bea
devdilshan2025 <dsbandara4@gmail.com>
2025-11-16 10:51:45 +0530

    create get all student API

--- src/main/java/edu/icet/Controller/StudentController.java
+++ src/main/java/edu/icet/Controller/StudentController.java
@@ -4,8 +4,11 @@ import edu.icet.model.dto.Student;
 import edu.icet.repository.StudentRepository;
 import edu.icet.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 public class StudentController {
 
@@ -17,6 +20,14 @@ public class StudentController {
 
         studentService.add(student);
     }
+
+    @GetMapping("/all")
+    public List<Student> getAll(){
+
+        return studentService.getAll();
+    }
+
+
 
 
 }
--- src/main/java/edu/icet/service/StudentService.java
+++ src/main/java/edu/icet/service/StudentService.java
@@ -6,13 +6,16 @@ import edu.icet.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class StudentService {
 
     @Autowired
     StudentRepository studentRepository;
 
-    public void add(Student student){
+    public void add(Student student) {
         studentRepository.save(new StudentEntity(
                 student.getId(),
                 student.getName(),
@@ -22,4 +25,25 @@ public class StudentService {
                 student.getBirthday()
         ));
     }
+
+    public List<Student> getAll() {
+
+        List<StudentEntity> all = studentRepository.findAll();
+
+        List<Student> studentList = new ArrayList<>();
+
+        for (StudentEntity studentEntity : all) {
+            studentList.add(new Student(
+                    studentEntity.getId(),
+                    studentEntity.getName(),
+                    studentEntity.getAddress(),
+                    studentEntity.getEmail(),
+                    studentEntity.getPhone(),
+                    studentEntity.getBirthday()
+            ));
+        }
+        return studentList;
+    }
+
+
 }
